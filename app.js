90d774d821543564ac58cc922fdb0af2844f9ae8
Brian Pick <brianr.pick@gmail.com>
2018-01-03 20:29:52 -0800

    Working api call with key

--- app.js
+++ app.js
@@ -2,4 +2,21 @@
 //general http://www.omdbapi.com/?s=guardians+of+the+galaxy&apikey=thewdb
 //search http://www.omdbapi.com/?i=tt3896198&apikey=thewdb
 var express = require('express');
-var app = 
+var app = express();
+var request = require('request');
+
+app.get("/results", function(req, res){
+	request('http://www.omdbapi.com/?i=tt3896198&apikey=thewdb', function(err, response, body) {
+		if (!err && response.statusCode == 200) {
+			var parsedBody = JSON.parse(body)
+			res.send(parsedBody);
+		}
+	});
+})
+
+
+
+
+app.listen(3000, function() {
+	console.log("Serving app on port 3000");
+})
