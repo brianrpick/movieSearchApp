6557d97234429104427caf6ff9351a5a54bee5ab
Brian Pick <brianr.pick@gmail.com>
2018-01-03 20:40:30 -0800

    Displaying results from api json

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 //&apikey=thewdb
 //general http://www.omdbapi.com/?s=guardians+of+the+galaxy&apikey=thewdb
-//search http://www.omdbapi.com/?i=tt3896198&apikey=thewdb
+//search http://www.omdbapi.com/?s=golden&apikey=thewdb
 var express = require('express');
 var app = express();
 var request = require('request');
 
+app.set("view engine", "ejs");
+
 app.get("/results", function(req, res){
-	request('http://www.omdbapi.com/?i=tt3896198&apikey=thewdb', function(err, response, body) {
+	request('http://www.omdbapi.com/?s=golden&apikey=thewdb ', function(err, response, body) {
 		if (!err && response.statusCode == 200) {
 			var parsedBody = JSON.parse(body)
-			res.send(parsedBody);
+			res.render("results", {body: parsedBody});
 		}
 	});
 })
